77b3d5f187d6eb796945d768bc92a12ad5945db7
1MD0NK3Y <jacopino.toma@gmail.com>
2024-12-03 19:30:56 +0100

    ora vede se ci sono tabelle vicino

--- src/game/HitoriGame.py
+++ src/game/HitoriGame.py
@@ -8,59 +8,127 @@ from random import choice
 from libs.boardgamegui import gui_play
 
 
-intMatrix = list[list[int]]
-anyMatrix = list[list[any]]
-
 class HitoriGame(BoardGame):
     
-    def __init__(self, w: int, h: int):
+    def __init__(self, w: int, h: int) -> None:
         
         self._w, self._h = w, h
-        self._numbers = list(range(6 * 6))
-        self._annots = [0] * (6 * 6)
+        self._numbers = list(range(w * h))
+        self._annots = [0] * (w * h)
         
-        
-    def play(self, x, y, action):
+
+    def play(self, x, y, action) -> None:
+
         if action == "flag":
+
             self.fill(x, y)
+
         else:
+
             self._annots[x + y * self._w] += 1
             self._annots[x + y * self._w] %= 3
 
 
-    def read(self, x, y):
+    def read(self, x: int, y: int) -> str:
+
         txt = str(self._numbers[x + y * self._w])
+
         if self._annots[x + y * self._w] == 1:
+
             txt += "#"
+
         elif self._annots[x + y * self._w] == 2:
+
             txt += "!"
+
         return txt
 
 
-    def fill(self, x, y):
+    def fill(self, x: int, y: int):
+
         bd, w, h = self._annots, self._w, self._h
+
         if 0 <= x < w and 0 <= y < h and bd[x + y * w] == 0:
+
             bd[x + y * w] = 1
+
             for dx, dy in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
+
                 self.fill(x + dx, y + dy)
 
-    
-    def finished(self): 
+
+    def finished(self) -> bool: 
+        
+        for x in range(self._w):
+            
+            for y in range(self._h):
+                
+                try:
+                    
+                    if (self._annots[x + y * self._w] == 1 and self._annots[(x+1) + y * self._w] == 1):
+                        
+                        print("LOL")
+                        
+                        return False
+                
+                except:
+                    
+                    pass
+                
+                # NON SERVE
+                try:
+                    
+                    if (self._annots[x + y * self._w] == 1 and self._annots[(x-1) + y * self._w] == 1):
+                        
+                        print("LMAO")
+                        
+                        return False
+                
+                except:
+                    
+                    pass
+                
+                
+                try:
+                    
+                    if (self._annots[x + y * self._w] == 1 and self._annots[x + (y+1) * self._w] == 1):
+                        
+                        print("XD")
+                        
+                        return False
+                
+                except:
+                    
+                    pass
+                
+                # NON SERVE
+                try:
+                    
+                    if (self._annots[x + y * self._w] == 1 and self._annots[x + (y-1) * self._w] == 1):
+                        
+                        print(":)")
+                        
+                        return False
+                
+                except:
+                    
+                    pass
+
         
         return False
+
     
-    
-    def status(self): 
+    def status(self):
         
         return "Playing"
+
     
-    
-    def cols(self): 
+    def cols(self) -> int: 
         
         return self._w
+
     
-    
-    def rows(self): 
+    def rows(self) -> int: 
         
         return self._h
         
@@ -68,6 +136,6 @@ class HitoriGame(BoardGame):
         
 if __name__ == "__main__":
     
-    hi = HitoriGame(5, 5)
+    hi = HitoriGame(6, 6)
     
     gui_play(hi)
--- src/libs/boardgamegui.py
+++ src/libs/boardgamegui.py
@@ -4,60 +4,71 @@
 @license This software is free - https://opensource.org/license/mit
 """
 
-#try:
     
-#    from __main__ import g2d
-        
-#except:
-    
-#    import libs.g2d
-
 from libs.boardgame import BoardGame
 from libs.g2d import *
 
 W, H = 40, 40
+BLACK, GRAY, WHITE = (0, 0, 0), (127, 127, 127), (255, 255, 255)
 
 class BoardGameGui:
-    def __init__(self, game: BoardGame):
+    def __init__(self, game: BoardGame,
+                 actions={"LeftButton": "", "RightButton": "flag"},
+                 annots={"#": (0, GRAY), "!": (2, GRAY)}):
         self._game = game
+        self._actions = actions
+        self._annots = annots
         self.update_buttons()
 
     def tick(self):
         game = self._game
-        x, y = mouse_pos()
-        bx, by = x // W, y // H
+        mouse_x, mouse_y = mouse_pos()
+        x, y = mouse_x // W, mouse_y // H
         released = set(previous_keys()) - set(current_keys())
         if game.finished():
             alert(game.status())
             close_canvas()
-        elif "Escape" in released:  # "Escape" key released
+            return
+        if "Escape" in released:  # "Escape" key released
             close_canvas()
-        elif "LeftButton" in released and by < game.rows():
-            game.play(bx, by, "")
-            self.update_buttons()
-        elif "RightButton" in released and by < game.rows():
-            game.play(bx, by, "flag")
-            self.update_buttons()
+            return
+        for k, v in self._actions.items():
+            if k in released and y < game.rows():
+                game.play(x, y, v)
+                self.update_buttons((x, y))
 
-    def update_buttons(self):
+    def update_buttons(self, last_move=None):
         cols, rows = self._game.cols(), self._game.rows()
-        clear_canvas()
-        set_color((0, 0, 0))
-        for y in range(1, rows + 1):
-            draw_line((0, y * H), (cols * W, y * H))
-        for x in range(1, cols):
-            draw_line((x * W, 0), (x * W, rows * H))
+        clear_canvas(BLACK)
         for y in range(rows):
             for x in range(cols):
-                _write(self._game.read(x, y), x * W, y * H, W, H)
+                text = self._game.read(x, y)
+                self.write(text, (x, y))
         status = self._game.status()
-        _write(status, 0, rows * H, cols * W, H)
-
-def _write(text, x, y, w, h):
-    fsize = 0.75 * min(h, 2 * w / len(text or " "))
-    draw_text(text, (x + w // 2, y + h // 2), fsize)
+        self.write(status, (0, rows), cols)
+        
+    def write(self, text, pos, cols=1):
+        x, y = pos        
+        set_color(WHITE)
+        draw_rect((x * W + 1, y * H + 1), (cols * W - 2, H - 2))
+        
+        last = text[-1:]
+        if cols == 1 and last in self._annots:
+            stroke, color = self._annots[last]
+            set_stroke(stroke)
+            set_color(color)
+            draw_circle((x * W + W / 2, y * H + W / 2), min(W, H) / 2 - 2)
+            set_stroke()
+            text = text[:-1]
+        
+        chars = max(1, len(text))
+        fsize = min(0.75 * H, 1.5 * cols * W / chars)
+        center = (x * W + cols * W/2, y * H + H/2)
+        set_color(BLACK)
+        draw_text(text, center, fsize)
 
 def gui_play(game: BoardGame):
     init_canvas((game.cols() * W, game.rows() * H + H))
     ui = BoardGameGui(game)
     main_loop(ui.tick)
+
